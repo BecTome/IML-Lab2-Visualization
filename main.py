e499a2a024af15a5ff6f4b62a03fc7f9d1970a21
Alberto Becerra <alberto.becerra.tome@gmail.com>
2023-11-10 18:55:47 +0100

    Restructure

--- main.py
+++
@@ -1,6 +0,0 @@
-from src.analysis.glass import dbscan_report, birch_report, pam_report,\
-                                clara_report, fuzzycmeans_report
-from src.analysis.vote import dbscan_report, birch_report, pam_report,\
-                                clara_report, fuzzycmeans_report
-from src.analysis.heart_h import dbscan_report, birch_report, pam_report,\
-                                clara_report, fuzzycmeans_report
